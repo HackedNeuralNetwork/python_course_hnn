5cf71a030322a7703d59052a0829103298dfd886
Hacked Neural Network <info.hackedneuralnetwork@gmail.com>
2022-07-20 17:40:58 +0530

    Python Basic Code Folder

---
+++ Python Basic/Variable_keywords.py
@@ -0,0 +1,58 @@
+# What is keywords ?
+import keyword
+
+"""
+Comments: For Human Reading
+['False', 'None', 'True', '__peg_parser__', 'and', 'as', 'assert', 
+'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 
+'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 
+'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 
+'while', 'with', 'yield']
+Total = 36
+"""
+# print all keywords
+print(keyword.kwlist)
+
+# print total number of keywords
+print(len(keyword.kwlist))
+
+"""
+# What is Variables ?
+# maths
+X = 5 value of x is 5
+X: Variable
+1) Never Create anything using Capital Letter (Only Class)
+"""
+name = "Yogendra"
+age = 26
+print(name)
+
+"""
+Types of Variable
+
+Number = Int = 1,2,3,4,5
+point value = Float = 45.6Kg, 8.9L
+Alfa = String = A,B,C,D
+Bool = True or False
+"""
+name = 16816
+print(name)
+
+"""
+How to create a Variable
+1) small letter
+2) Not allow = start with number
+Eg: 1name (Wrong Way)
+Right way = name1, name_1 
+3) Variable are case sen.
+    name
+    Name
+    NAME
+    nAME
+    naME
+    NaMe
+    namE  
+4) Never Give Space 
+Eg: full name (Wrong Way) there are 2 var
+Right Way:= full_name, fullName, fullname
+"""
