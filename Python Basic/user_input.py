6ba37aecdf7490c64d7addecd6126cd3e19429fa
Hacked Neural Network <info.hackedneuralnetwork@gmail.com>
2022-07-20 21:17:14 +0530

    Get Data From User using user Input

---
+++ Python Basic/user_input.py
@@ -0,0 +1,12 @@
+"""
+User input: Get Data From User
+input() : String
+"""
+
+name = input("Enter Your Name: ")
+age = input("Enter Your Age: ")
+mobile_number = input("Enter Your Mobile Number: ")
+
+print("Name: ",name)
+print("Age: ",age)
+print("Mobile Number: ", mobile_number)
