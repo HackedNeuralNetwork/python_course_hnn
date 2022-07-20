bc3f29709cfd03b6fb51f5f19da67458a7fbc461
Hacked Neural Network <info.hackedneuralnetwork@gmail.com>
2022-07-20 19:02:57 +0530

    Data Type & Memory Management

---
+++ Python Basic/data_type.py
@@ -0,0 +1,40 @@
+"""
+Data Type
+
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+"""
+
+# Create String
+name = "Yogendra"
+lastName = 'Rathore'
+email_id = "yogendra2022@gmail.com"
+
+# Int
+# number = 7023653566
+
+"""
+Memory Management
+RAM = 4GB,6GB,8GB,etc
+"""
+print("First: ",name)
+print(id(name))
+print(type(name))
+name = 7023653566
+print("Second: ",name)
+print(id(name))
+print(type(name))
+"""
+Last ID:
+Yogendra
+2106428432048
+
+Current ID:
+1404906446512
+"""
