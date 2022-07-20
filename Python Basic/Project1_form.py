d81660044a44d311986525a3c0e7e7a6a11f8baf
Hacked Neural Network <info.hackedneuralnetwork@gmail.com>
2022-07-20 18:02:51 +0530

    Project 1 Create Form

---
+++ Python Basic/Project1_form.py
@@ -0,0 +1,9 @@
+name = "Yogendra Singh"
+age = 26
+company_name = "Hacked Neural Network"
+mobileNumber = 7023653566
+
+print("Name:- ",name)
+print("Age:- ",age)
+print("Company Name:- ",company_name)
+print("Mobile Number",mobileNumber)
