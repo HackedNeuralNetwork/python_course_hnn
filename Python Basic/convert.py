e871499c50edb561636698095c3084a289bdd699
Hacked Neural Network <info.hackedneuralnetwork@gmail.com>
2022-07-25 11:24:32 +0530

    Project 1 Create Form

---
+++ Python Basic/convert.py
@@ -0,0 +1,19 @@
+"""
+Convert
+"""
+
+name = input("Enter Your Name: ")
+age = input("Enter Your Age: ")
+age = int(age)
+print(age,type(age))
+age = float(age)
+print(age,type(age))
+age = str(age)
+print(age,type(age))
+mobile_number = input("Enter Your Mobile Number: ")
+mobile_number = int(mobile_number)
+
+print("Name: ",name)
+print("Age: ",age)
+print(type(age))
+print("Mobile Number: ", mobile_number)
--- Python Basic/user_input.py
+++ Python Basic/user_input.py
@@ -1,12 +1,26 @@
 """
 User input: Get Data From User
 input() : String
+
+*******Important *******
+input(),type(),id() ==> Functions
+name,address = Small Letter ( Variable )
+Name = First letter Capital ( Class )
+
+*********** Important Convert *************
+string 'A' not convert into number
+string '1' convert into number using int() ==> 1
+string '56.5' convert into float using float() ==? 56.5
 """
 
-name = input("Enter Your Name: ")
-age = input("Enter Your Age: ")
-mobile_number = input("Enter Your Mobile Number: ")
+name = input("Enter Your Name Here: ")
+age = int(input("Enter Your Age Here: "))  # String to int
+company_name = input("Enter Your Company Name: ")
+mobileNumber = input("Enter Your Mobile Number: ")
+mobileNumber = int(mobileNumber) # String to int
 
-print("Name: ",name)
-print("Age: ",age)
-print("Mobile Number: ", mobile_number)
+print("Name:- ",name)
+print("Age:- ",age)
+print("Company Name:- ",company_name)
+print("Mobile Number",mobileNumber)
+print(type(age),age)
